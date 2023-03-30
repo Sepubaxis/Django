8b0baced84f6eaf65e19c65aac9e16f2970f2857
Sepubaxis <114174171+Sepubaxis@users.noreply.github.com>
2023-03-30 18:28:47 -0300

    Add files via upload

---
+++ PROJECT DJANGO/Crodisc/base/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+from base.models import Room, Topic, Message
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        
+        
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['name']
+        
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
